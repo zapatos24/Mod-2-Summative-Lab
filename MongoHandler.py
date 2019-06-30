5c3273919ef10739adaca273db0111b40c541821
Jeremy Owens <jeremy.traber.owens@gmail.com>
2019-06-30 18:09:40 -0400

    sql and mongo handlers fully functional, ready for integration of API work

--- MongoHandler.py
+++ MongoHandler.py
@@ -33,12 +33,18 @@ class MongoHandler():
 
     # Create dictionary for entry into MongoDB from Pandas DataFrame
     def make_dict_from_df(DataFrame, rownum):
+        season = str(int(DataFrame.Season.iloc[rownum]))
         team_to_add = {'name': DataFrame.index[rownum],
-                       '2011_goals': int(DataFrame.iloc[rownum].tot_home_goals),
-                       '2011_wins': int(DataFrame.iloc[rownum].tot_home_win),
-                       '2011_loss': int(DataFrame.iloc[rownum].tot_home_loss),
-                       '2011_draw': int(DataFrame.iloc[rownum].tot_home_draw),
-                       '2011_graph': DataFrame.iloc[rownum].graph
+                       season+'_goals': int(DataFrame.iloc[rownum].GoalsScored),
+                       season+'_wins': int(DataFrame.iloc[rownum].Wins),
+                       season+'_losses': int(DataFrame.iloc[rownum].Losses),
+                       season+'_draws': int(DataFrame.iloc[rownum].Draws),
+                       season+'_raingames': int(DataFrame.iloc[rownum].RainGames),
+                       season+'_nonraingames': int(DataFrame.iloc[rownum].NonRainWins),
+                       season+'_rainwin%': float(DataFrame.iloc[rownum]['RainWin%']),
+                       season+'_nonrainwin%': float(DataFrame.iloc[rownum]['NonRainWin%']),
+                       season+'_change%': float(DataFrame.iloc[rownum]['%ChangeWinWithRain']),
+                       'graph': DataFrame.iloc[rownum].graph
                        }
         return team_to_add
 
@@ -51,10 +57,11 @@ class MongoHandler():
         return list_of_teams
 
     # Query the database, makes sure no one tries to call the graph
-    # (because .ipynb hates it)
-    def query_db(self, db, collName, wdict={'graph': 0}, sdict={'graph': 0}):
+    # (because Jupyter Notebooks hate it)
+    def query_db(self, db, collName, wdict={}, sdict={'_id': 0, 'graph': 0}):
         if sdict['graph'] != 0:
-            print('Please do not query the graph, it breaks things in .ipynb')
+            print("Please don't query the graph, Jupyter Notebooks get cranky")
+            return []
         else:
             query = self.client[db][collName].find(wdict, sdict)
             return query
