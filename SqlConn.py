4930194dd6bf6f44e567253de5e5581843db7b96
Jeremy Owens <jeremy.traber.owens@gmail.com>
2019-07-01 11:23:27 -0400

    added panda handler

--- SqlConn.py
+++ SqlConn.py
@@ -3,17 +3,21 @@ import pandas as pd
 
 
 class SqlConn:
+    # Initializes the object with a connection name and cursor for use in the
+    # desired SQL Database
     def __init__(self, database_name):
         self.conn = sqlite3.connect(database_name)
         self.c = self.conn.cursor()
         self.connstat = 'Active'
         print("Connection status: " + self.connstat)
 
-    def matches_df(self, season):
+    # Takes in a list of seasons and returns a DataFrame from the Matches table
+    # of the Division 1 and 2 matches from the specified seasons
+    def matches_df(self, list_of_seasons):
         self.c.execute("""SELECT *
                           FROM Matches
                           WHERE Season IN (""" +
-                       str(season) +
+                       ', '.join([str(year) for year in list_of_seasons]) +
                        """) AND Div IN ('D1','D2')
                           ORDER BY Date""")
 
@@ -22,6 +26,7 @@ class SqlConn:
         print("Connection status: " + self.connstat)
         return df
 
+    # Closes the connection to the SQL database
     def close_conn(self):
         print("Closing connection")
         self.c.close()
